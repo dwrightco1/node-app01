ae49d92b0183e5301c7eb83148fcb282d1a893f3
Dan Wright <dan@dyvantage.com>
2020-11-01 01:57:05 +0000

    update

--- server.js
+++ server.js
@@ -2,6 +2,38 @@ const http = require('http')
 const fs = require('fs')
 const port = process.env.PORT || 80
 
+//var mysql = require('mysql');
+//var con = mysql.createConnection({
+//  host: "172.17.0.3",
+//  user: "root",
+//  password: "Passw0rd",
+//  database: "nodeapp"
+//});
+
+//con.connect(function(err) {
+//  if (err) throw err;
+//  con.query("SELECT * FROM appconfig", function (err, result, fields) {
+//    if (err) throw err;
+//    console.log(result);
+//  });
+//});
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: '172.17.0.3',
+	user: 'root',
+	password: 'Passw0rd',
+	database: 'nodeapp'
+});
+
+connection.query('SHOW TABLES',function (error, rows, fields) {
+    if (error)
+        return console.log(error);
+    console.log(rows);
+});
+
+connection.end();
+
 fs.readFile('./index.html', function (err, html) {
     if (err) {
         throw err;
