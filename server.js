ced0946848120d017dc7f411dc2f9dd6bef8a48b
Dan Wright <dan@dyvantage.com>
2020-11-01 14:49:22 +0000

    update

--- server.js
+++ server.js
@@ -1,38 +1,25 @@
 const http = require('http')
 const fs = require('fs')
 const port = process.env.PORT || 81
+const database_ip = process.env.DATABASE_IP || "172.17.0.2"
 
-//var mysql = require('mysql');
-//var con = mysql.createConnection({
-//  host: "172.17.0.3",
-//  user: "root",
-//  password: "Passw0rd",
-//  database: "nodeapp"
-//});
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: '172.17.0.2',
+	user: 'root',
+	password: 'Passw0rd',
+	database: 'nodeapp'
+});
 
-//con.connect(function(err) {
-//  if (err) throw err;
-//  con.query("SELECT * FROM appconfig", function (err, result, fields) {
-//    if (err) throw err;
-//    console.log(result);
-//  });
-//});
+var mysql_return = ""
+connection.query('SHOW TABLES',function (error, rows, fields) {
+    if (error)
+        return console.log(error);
+    console.log(rows);
+    mysql_return = rows
+});
 
-//var mysql = require('mysql');
-//var connection = mysql.createConnection({
-//	host: '172.17.0.3',
-//	user: 'root',
-//	password: 'Passw0rd',
-//	database: 'nodeapp'
-//});
-
-//connection.query('SHOW TABLES',function (error, rows, fields) {
-//    if (error)
-//        return console.log(error);
-//    console.log(rows);
-//});
-
-//connection.end();
+connection.end();
 
 fs.readFile('./index.html', function (err, html) {
     if (err) {
@@ -40,6 +27,7 @@ fs.readFile('./index.html', function (err, html) {
     }
     http.createServer(function(request, response) {
         response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(mysql_return);
         response.write(html);
         response.end();
     }).listen(port);
