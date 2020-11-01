8591be12c2a38a6d4612e8c4ec9359f71d9dd568
vagrant <vagrant@vagrant.vm>
2020-11-01 19:57:44 +0000

    update

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
+// required modules
 const http = require('http')
 const fs = require('fs')
-const port = process.env.NODE_PORT || 81
-const database_ip = process.env.DATABASE_IP || "ip-not-specified"
-const database_user = process.env.DATABASE_USER || "user-not-specified"
-const database_pw = process.env.DATABASE_PW || "password-not-specified"
-const database_name = process.env.DATABASE_NAME || "instance-not-specified"
-
 var mysql = require('mysql');
 
-console.log("NODEAPP: initializing database connection: " + database_user + "@" + database_ip + ":" + database_name)
-var con = mysql.createConnection({
-  host: database_ip,
-  user: database_user,
-  password: database_pw,
-  database: "nodeapp"
-});
+// initialize web server & database parameters
+const port = process.env.NODE_PORT || 80
+const database_ip = process.env.DATABASE_IP || "unset"
+const database_user = process.env.DATABASE_USER || "unset"
+const database_pw = process.env.DATABASE_PW || "unset"
+const database_name = process.env.DATABASE_NAME || "unset"
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("NODEAPP: querying database")
-  con.query("SELECT * FROM appconfig", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
+// update console
+console.log("NODEAPP: starting http listener on port " + port)
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    console.log("NODEAPP: starting http listener on port " + port)
-    http.createServer(function(request, response) {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }).listen(port);
-});
+// create http server
+http.createServer(function(nodeapp_request, nodeapp_response) {
+    // read database (synchronously)
+    console.log(`--> querying database ${database_user}@${database_ip}:${database_name}`)
+    var con = mysql.createConnection({
+      host: database_ip,
+      user: database_user,
+      password: database_pw,
+      database: "nodeapp"
+    });
+
+    con.connect(function(mysql_err) {
+      //if (mysql_err) throw mysql_err;
+      console.log("NODEAPP: querying database")
+      //con.query("SELECT * FROM appconfig", function (select_err, result, fields) {
+      //  if (select_err) throw select_err;
+      //  console.log(result);
+      //});
+    });
+
+    // write header & response
+    nodeapp_response.writeHeader(200, {"Content-Type": "text/html"});
+    nodeapp_response.end('Hello\n');
+}).listen(port);
